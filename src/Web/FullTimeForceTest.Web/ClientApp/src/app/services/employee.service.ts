9c501c6a47310150fe70af9e7c06af8923d5dee6
orlando_lt16 <orlando_lt16@hotmail.com>
2020-03-03 16:00:03 -0500

    add services

---
+++ src/Web/FullTimeForceTest.Web/ClientApp/src/app/services/employee.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from './base.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService extends BaseService {
+
+  constructor(private http: HttpClient) {
+      super();
+  }
+
+  //saveEmployeeCalculateSalary(): Observable<boolean> {
+    
+  //}
+
+
+
+}

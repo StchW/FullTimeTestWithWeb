1376a746c2f6056c702501dc95774ae81c5976a6
orlando_lt16 <orlando_lt16@hotmail.com>
2020-03-03 16:17:15 -0500

    add menu

--- src/Web/FullTimeForceTest.Web/ClientApp/src/app/app-routing.module.ts
+++ src/Web/FullTimeForceTest.Web/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { StudentComponent } from './components/student/student.component';
+import { WordComponent } from './components/word/word.component';
 
 
 const routes: Routes = [
-  //{ path: "storyUpdate/:id", component: StoryUpdateComponent }
+  { path: "employee", component: EmployeeComponent },
+  { path: "student", component: StudentComponent },
+  { path: "word", component: WordComponent }
 ];
 
 @NgModule({
